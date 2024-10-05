f2fd2b746cc7934232897c1df67f295ecb51e4c5
Ritu Raj Pandey <44659502+Riturajpandey9204@users.noreply.github.com>
2024-10-05 16:38:42 +0530

    Add files via upload

---
+++ 01.1_KES_Admin_Module_Caseworker_Account_Creation/src/main/java/in/jt/enums/ProjectEnum.java
@@ -0,0 +1,7 @@
+package in.jt.enums;
+
+public class ProjectEnum {
+	public static final String SUCCESS = "Your account has created successfully....!!!!";
+	public static final String FAIL = "Your account has not created....!!!!";
+
+}
